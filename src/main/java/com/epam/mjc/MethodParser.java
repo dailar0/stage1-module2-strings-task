cc1f415ec5ddc23662fcb7134524d8fc9ee1580d
dailar <dailarse00@gmail.com>
2023-02-03 13:02:56 +0300

    done

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,25 +1,60 @@
 package com.epam.mjc;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MethodParser {
 
     /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
      * signatureString is a java-like method signature with following parts:
-     *      1. access modifier - optional, followed by space: ' '
-     *      2. return type - followed by space: ' '
-     *      3. method name
-     *      4. arguments - surrounded with braces: '()' and separated by commas: ','
+     * 1. access modifier - optional, followed by space: ' '
+     * 2. return type - followed by space: ' '
+     * 3. method name
+     * 4. arguments - surrounded with braces: '()' and separated by commas: ','
      * Each argument consists of argument type and argument name, separated by space: ' '.
      * Examples:
-     *      accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
-     *      private void log(String value)
-     *      Vector3 distort(int x, int y, int z, float magnitude)
-     *      public DateTime getCurrentDateTime()
+     * accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
+     * private void log(String value)
+     * Vector3 distort(int x, int y, int z, float magnitude)
+     * public DateTime getCurrentDateTime()
      *
      * @param signatureString source string to parse
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        String signatureWOArgs = signatureString.split("\\(.*\\)")[0];
+        String args = signatureString.substring(signatureWOArgs.length());
+
+        String[] signature = signatureWOArgs.split("\\s");
+        boolean isAccessModified = signature.length > 2;
+
+        MethodSignature methodSignature;
+
+        StringSplitter stringSplitter = new StringSplitter();
+        List<String> arguments = stringSplitter.splitByDelimiters(args, List.of(",", "(", ")"));
+        List<MethodSignature.Argument> argumentList = arguments
+                .stream()
+                .map(s -> {
+                    String[] pair = s.trim().split(" ");
+                    return new MethodSignature.Argument(pair[0], pair[1]);
+                })
+                .collect(Collectors.toList());
+
+        if (isAccessModified) {
+            String accessModifier = signature[0];
+            String returnType = signature[1];
+            String methodName = signature[2];
+            methodSignature = new MethodSignature(methodName, argumentList);
+            methodSignature.setReturnType(returnType);
+            methodSignature.setAccessModifier(accessModifier);
+        } else {
+            String returnType = signature[0];
+            String methodName = signature[1];
+            methodSignature = new MethodSignature(methodName, argumentList);
+            methodSignature.setReturnType(returnType);
+        }
+
+        return methodSignature;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,18 +1,25 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class StringSplitter {
 
     /**
      * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        String delims = String.join("", delimiters);
+        StringTokenizer stringTokenizer = new StringTokenizer(source, delims);
+        ArrayList<String> strings = new ArrayList<>();
+        while (stringTokenizer.hasMoreTokens())
+            strings.add(stringTokenizer.nextToken());
+        return strings;
     }
 }
